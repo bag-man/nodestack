08c7c4fcb4adc33b59504091c86b0259be374c4b
Owen Garland <owen.garland@clock.co.uk>
2017-03-09 15:27:44 +0000

    Testing looks complete

--- test/mongoose.test.js
+++ test/mongoose.test.js
@@ -1,15 +1,13 @@
 const dbUrl = 'mongodb://localhost/test'
     , FooModel = require('../app/dbschema.js')(dbUrl)
-    // , Database = require('../app/dbmongoose.js')
     , assert = require('assert')
-    , testData = { foo: 10, bar: 'foo' }
 
 describe('Database', () => {
-  // afterEach(() => {
-  //   let url = dbUrl
-  //     , db = new Database(url)
-  //   db.dropDatabase()
-  // })
+  let testData = { foo: 10, bar: 'foo' }
+
+  afterEach(() => {
+    FooModel.db.dropDatabase()
+  })
 
   it('should create a new model', (done) => {
     let testModel = new FooModel(testData)
