00a09b182b182a26ec7e0b63b20ab4e60303608d
Owen Garland <owen.garland@clock.co.uk>
2017-03-09 18:22:32 +0000

    Updated webpack config formatting

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,34 +1,28 @@
 const path = require('path')
 const webpack = require('webpack')
 
-module.exports = {
-  devtool: 'source-map'
-, entry: [
-    './app/assets/js/index'
-  ]
-, output: {
-    path: path.join(__dirname, 'app', 'assets', 'build', 'js')
-  , filename: 'bundle.js'
-  , publicPath: '/app/assets/build/js/'
-  }
-, plugins: [
-    new webpack.optimize.DedupePlugin()
-  , new webpack.optimize.UglifyJsPlugin({
-      minimize: true
-    , compress: {
-        warnings: false
-      }
-    })
-  ]
-, module: {
-    loaders: [
-      { test: /\.js$/
-      , loader: 'babel-loader'
-      , query: {
-          presets: [ 'es2015' ]
-        }
-      , include: path.join(__dirname, 'app/assets/js')
-      }
+module.exports =
+  { devtool: 'source-map'
+  , entry: [ './app/assets/js/index' ]
+  , output:
+    { path: path.join(__dirname, 'app', 'assets', 'build', 'js')
+    , filename: 'bundle.js'
+    , publicPath: '/app/assets/build/js/'
+    }
+  , plugins:
+    [ new webpack.optimize.DedupePlugin()
+    , new webpack.optimize.UglifyJsPlugin(
+      { minimize: true
+      , compress: { warnings: false }
+      })
     ]
+  , module:
+    { loaders:
+      [ { test: /\.js$/
+        , loader: 'babel-loader'
+        , query: { presets: [ 'es2015' ] }
+        , include: path.join(__dirname, 'app/assets/js')
+        }
+      ]
+    }
   }
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,20 @@
 const path = require('path')
 
-module.exports = {
-  entry: [
-    './app/assets/js/index'
-  ]
-, output: {
-    path: path.join(__dirname, 'app', 'assets', 'build', 'js')
-  , filename: 'bundle.js'
-  , publicPath: '/app/assets/build/js/'
-  }
-  , module: {
-    loaders: [
-      { test: /\.js$/
-      , loader: 'babel-loader'
-      , query: {
-          presets: [ 'es2015' ]
+module.exports =
+  { entry: [ './app/assets/js/index' ]
+  , output:
+    { path: path.join(__dirname, 'app', 'assets', 'build', 'js')
+    , filename: 'bundle.js'
+    , publicPath: '/app/assets/build/js/'
+    }
+  , module:
+    { loaders:
+      [ { test: /\.js$/
+        , loader: 'babel-loader'
+        , query: { presets: [ 'es2015' ] }
+        , include: path.join(__dirname, 'app/assets/js')
         }
-      , include: path.join(__dirname, 'app/assets/js')
-      }
-    ]
+      ]
+    }
   }
-}
 
